bc3ecec9e5b550e96e1fd718865eca3883b65e0d
ivo7ga <ivan_gonzalez_tsapp1oa2021@cev.com>
2022-02-14 21:40:10 +0100

    Initial Commit

---
+++ Target/TargetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TargetApp.swift
+//  Target
+//
+//  Created by Iván González on 14/2/22.
+//
+
+import SwiftUI
+
+@main
+struct TargetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
